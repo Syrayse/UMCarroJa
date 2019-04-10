1cd2ba4ff11833946183fae071216f74a8a5b0f9
RIBONTONE <totoguerra1@hotmail.com>
2019-04-10 17:47:51 +0100

    adicionei umas cenas a classe pessoa mas ainda nao ta tudo

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -7,8 +7,22 @@ public class Pessoa {
     private String password;
     private String morada;
     private Date dataNascimento;
-    //private int tipo??
-    //este int era o identificador do tipo de pessoa de que se trata, 1 para cliente, 2 para proprietario?????
+
+    public String getMail(){ return this.mail; }
+    public String getNome(){ return this.nome; }
+    public String getPassword(){ return this.password; }
+    public String getMorada(){ return this.morada; }
+    public Date getDataNascimento(){return this.dataNascimento;}
+
+    public void setMail(String x){ this.mail=x;}
+    public void setNome(String x){ this.nome=x;}
+    public void setPassword(String x){ this.password=x;}
+    public void setMorada(String x){ this.morada; }
+    public void setDataNascimento(Date x){
+        Date k = new Date(x);
+        this.dataNascimento=k;
+    }
+
 
 
 }
