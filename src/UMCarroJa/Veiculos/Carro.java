e9a4d0ff54efdb223e79a025a50049c4d24cbe7a
RIBONTONE <totoguerra1@hotmail.com>
2019-04-24 23:01:45 +0100

    cenas

--- src/UMCarroJa/Veiculos/Carro.java
+++ src/UMCarroJa/Veiculos/Carro.java
@@ -3,4 +3,21 @@ package UMCarroJa.Veiculos;
 import UMCarroJa.Interfaces.Abastecivel;
 
 public abstract class Carro extends Veiculo implements Abastecivel {
+    private String fazMOVER;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/UMCarroJa/Veiculos/CarroCombustao.java
+++ src/UMCarroJa/Veiculos/CarroCombustao.java
@@ -1,4 +1,32 @@
 package UMCarroJa.Veiculos;
 
 public class CarroCombustao extends Carro {
+
+    private int qtdActual;
+    private int qtdMax;
+
+    public CarroCombustao(){
+        super();
+        this.qtdActual=0;
+        this.qtdMax=0;
+    }
+
+    public CarroCombustao (){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
