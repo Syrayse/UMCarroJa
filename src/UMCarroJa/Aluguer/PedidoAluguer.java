46c0637c1a22fe7dafa24bd8e74617f9390bfe5f
Syrayse <ruipedronetoreis12@gmail.com>
2019-04-25 11:39:48 +0100

    Fix bug no PedidoAluguer

--- src/UMCarroJa/Aluguer/PedidoAluguer.java
+++ src/UMCarroJa/Aluguer/PedidoAluguer.java
@@ -1,11 +1,11 @@
 package UMCarroJa.Aluguer;
 
-import UMCarroJa.PublicInfo.InfoPublicaCliente;
+import UMCarroJa.PublicInfo.InfoPublicaPessoa;
 import UMCarroJa.lib.Localizacao;
 
 public class PedidoAluguer
 {
-    private InfoPublicaCliente informacaoCliente;
+    private InfoPublicaPessoa informacaoCliente;
     private long idVeiculo;
     private Localizacao origem;
     private Localizacao destino;
