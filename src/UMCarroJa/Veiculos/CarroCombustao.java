50f4a577146f1a0b97a9a96c63d3700249e037ae
RIBONTONE <totoguerra1@hotmail.com>
2019-04-25 11:33:18 +0100

    carro combustao e carro

--- src/UMCarroJa/Veiculos/CarroCombustao.java
+++ src/UMCarroJa/Veiculos/CarroCombustao.java
@@ -1,5 +1,7 @@
 package UMCarroJa.Veiculos;
 
+import UMCarroJa.lib.Localizacao;
+
 public class CarroCombustao extends Carro {
 
     private int qtdActual;
@@ -11,22 +13,53 @@ public class CarroCombustao extends Carro {
         this.qtdMax=0;
     }
 
-    public CarroCombustao (){
+    public CarroCombustao (long idVeiculo, long idProprietario, double velocidadePorKM,
+                           String matricula, String marca, String modelo, double precoPorKM,
+                   Localizacao localizacao, boolean temEspera, String fazmovvv,int qtdActual,int qtdMax){
+
+        super(idVeiculo,idProprietario, velocidadePorKM,
+        matricula,  marca, modelo,precoPorKM,
+        localizacao, temEspera,fazmovvv);
+        this.qtdMax=qtdMax;
+        this.qtdActual=qtdActual;
 
     }
 
+    public CarroCombustao(CarroCombustao outro){
+        super(outro);
+        this.qtdActual=outro.getQtdActual();
+        this.qtdMax=outro.getQtdMax();
+    }
 
 
+    public int getQtdActual(){ return this.qtdActual; }
+    public int getQtdMax(){ return this.qtdMax;}
 
+    public void setQtdActual(int x){ this,qtdActual=x;}
+    public void setQtdMax(int x){ this.qtdMax=x;}
 
+    public String toString(){
 
+        StringBuilder sb = new StringBuilder();
 
+        sb.append(super.toString());
+        sb.append("Qtd actual de combustivel = ").append(this.qtdActual);
+        sb.append("Qtd Max de combustivel = ").append(this.qtdMax);
 
+        return sb.toString();
+    }
 
+    public boolean equals(Object o) {
+        if(this==o) return true;
+        if(this.getClass()!=o.getClass() || o==null) return false;
 
+        CarroCombustao k = (CarroCombustao) o;
 
+        return super.equals(k) && this.qtdActual==k.getQtdActual() && this.qtdMax==k.getQtdMax();
+    }
 
-
-
+    public CarroCombustao clone(){
+        return new CarroCombustao(this);
+    }
 
 }
