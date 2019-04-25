b3fb1b73f0705bbc7916c8fe11fec662bdf39043
Falape <filipe_s_fernandes@hotmail.com>
2019-04-25 12:59:31 +0100

    Classes de Proprietários e Clientes

--- src/UMCarroJa/Users/Cliente.java
+++ src/UMCarroJa/Users/Cliente.java
@@ -1,4 +1,56 @@
 package UMCarroJa.Users;
 
+import UMCarroJa.lib.Localizacao;
+
+import java.time.LocalDate;
+
 public class Cliente extends Pessoa {
+
+    private Localizacao localizacao;
+    private int destreza;
+
+    public Cliente (){
+        super();
+        this.localizacao = new Localizacao();
+        this.destreza = 0;
+    }
+
+    public Cliente (long id, String email, String nome, String password, String morada, LocalDate dataNascimento, int x, int y, double totalKm){
+        super(id, email, nome, password, morada, dataNascimento);
+        this.localizacao = new Localizacao(x,y);
+        this.destreza = 100;
+    }
+
+    public Cliente (Cliente cliente){
+        super(cliente);
+        this.localizacao= cliente.getLocalizacao();
+    }
+
+    public Localizacao getLocalizacao(){ return localizacao.clone(); }
+
+    public int getDestreza() {return this.destreza; }
+
+    public void setLocalizacao(Localizacao localizacao) { this.localizacao = localizacao; }
+
+
+    public String toString() {
+       StringBuilder sb = new StringBuilder();
+       sb.append(super.toString());
+       sb.append("Localização: ").append(this.localizacao.toString());
+       sb.append("Destreza: ").append(this.destreza);
+       return (sb.toString());
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return super.equals(cliente) &&
+                localizacao.equals(cliente.getLocalizacao()) &&
+                destreza==cliente.getDestreza();
+    }
+
+    public Cliente clone(){
+        return new Cliente(this);
+    }
 }
--- src/UMCarroJa/Users/Proprietario.java
+++ src/UMCarroJa/Users/Proprietario.java
@@ -2,9 +2,67 @@ package UMCarroJa.Users;
 
 import UMCarroJa.Interfaces.Classificavel;
 
+import java.time.LocalDate;
+
 public class Proprietario extends Pessoa implements Classificavel {
+
+    private long nClassificacao;
+    private double classificacao;
+
+    public Proprietario(){
+        super();
+        this.nClassificacao=0;
+        this.classificacao=0;
+    }
+
+    public Proprietario(long id, String email, String nome, String password, String morada, LocalDate dataNascimento){
+        super(id, email, nome, password, morada, dataNascimento);
+        this.nClassificacao= 0;
+        this.classificacao= 100;
+    }
+
+    public Proprietario(Proprietario proprietario){
+        super(proprietario);
+        this.nClassificacao=proprietario.getnClassifiacacao();
+        this.classificacao=proprietario.getClassificacao();
+    }
+
+    public double getClassificacao() {
+        return this.classificacao;
+    }
+
+    public long getnClassifiacacao(){
+        return this.nClassificacao;
+    }
+
+
     @Override
     public void classifica(double classificacao) {
+        long nAtual = this.nClassificacao;
+        double clAtual = this.classificacao;
+        this.classificacao = ((nAtual*clAtual + classificacao) / (nAtual + 1));
+        this.nClassificacao++;
+    }
 
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+        sb.append("Classificação: ").append(this.classificacao);
+        sb.append("Nº de classificações: ").append(this.nClassificacao);
+        return (sb.toString());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Proprietario proprietario = (Proprietario) o;
+        return super.equals(proprietario) &&
+                classificacao==proprietario.getClassificacao() &&
+                nClassificacao==proprietario.getnClassifiacacao();
+    }
+
+    public Proprietario clone(){
+        return new Proprietario(this);
     }
 }
